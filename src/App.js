866773c4a1bc308575eb81d1edc442607d458bb6
Sebastian <sebastian.stephenson1@gmail.com>
2022-08-26 14:34:25 -0400

    Added functionality that tells when you win or fail at the game

--- src/App.js
+++ src/App.js
@@ -5,15 +5,30 @@ import rock from './rock.svg';
 import scissors from './scissors.svg'
 
 function App() {
-  const[sign, setSign] = useState("");
+  const[sign, setSign] = useState("hey");
+  const [result, setResult] = useState('');
+
+  const randomSign = ['rock', 'paper', 'scissors'];
+
+  function check(param){
+    setSign(param);
+
+    if(randomSign[Math.floor(Math.random() * 3)] === param){
+      setResult('WIN!!!');
+    }else {
+      setResult("FAIL!!!");
+    }
+  }
+
   return (
     <>
       <h1 className='header'>Rock, Paper, Scissors?</h1>
-
+      
+      <h2>{result}</h2>
       <div className='signContainer'>
-        <img src={rock}></img>
-        <img src={paper}></img>
-        <img src={scissors}></img>
+        <img class='sign' onClick={() => {check("rock")}} src={rock}></img>
+        <img class='sign' onClick={() => {check("papaer")}} src={paper}></img>
+        <img class='sign'  onClick={() => {check("scissors")}} src={scissors}></img>
       </div>
     </>
   );
